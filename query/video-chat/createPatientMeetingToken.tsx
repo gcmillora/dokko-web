7c2937f812d16f5e8dfb04c4aa9a58ebf90a44bb
gcmillora <gcmillora@up.edu.ph>
2023-05-25 17:48:30 +0800

    temp. remove nbf exp for patient token

--- query/video-chat/createPatientMeetingToken.tsx
+++ query/video-chat/createPatientMeetingToken.tsx
@@ -1,21 +1,21 @@
-import { updatePatientMeetingToken } from './updatePatientMeetingToken';
+import { updatePatientMeetingToken } from "./updatePatientMeetingToken";
 
 export const createPatientMeetingToken = async (
   doctorUID: string,
   appointmentID: string,
   appointmentDate: Date
 ) => {
-  const data = fetch('https://api.daily.co/v1/meeting-tokens', {
-    method: 'POST',
+  const data = fetch("https://api.daily.co/v1/meeting-tokens", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Bearer 31b1e44009c810a075699272ddcbc6d9544cadd81244a1f7d6a22a0d1db55950`,
     },
     body: JSON.stringify({
       properties: {
         room_name: doctorUID,
-        nbf: new Date(appointmentDate).getTime() / 1000,
-        exp: new Date(appointmentDate).getTime() / 1000 + 3600,
+        // nbf: new Date(appointmentDate).getTime() / 1000,
+        // exp: new Date(appointmentDate).getTime() / 1000 + 1800,
       },
     }),
   })
@@ -24,6 +24,6 @@ export const createPatientMeetingToken = async (
       updatePatientMeetingToken(data.token, appointmentID);
     })
     .catch((error) => {
-      console.error('Error:', error);
+      console.error("Error:", error);
     });
 };
